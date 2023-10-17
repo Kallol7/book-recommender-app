206b97f928b6500255bc95baf5bb8a968ca57c18
Kallol Dhar <kalloldhar7@gmail.com>
2023-10-17 19:33:28 +0600

    login, signup pages designed

--- config/recommender/urls.py
+++ config/recommender/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("clean/", views.clean, name="clean"),
     path("update/", views.update, name="update"),
-    path("recommend/", views.recommend_view, name="recommend")
+    path("recommend/", views.recommend_view, name="recommend"),
+    path("signup/", views.signup_page, name="signup"),
+    path("login/", views.login_page, name="login")
 ]
--- config/recommender/views.py
+++ config/recommender/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .scripts import recommend
 from .scripts.create_one_json import create_one_json
 from .scripts.clean_one import clean_one
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -25,3 +26,20 @@ def recommend_view(request):
 
     csv_html = recommend.get_csv_as_html()
     return render(request, "recommender/recommend.html", {'csv_html': csv_html})
+
+def signup_page(request):
+    if request.method=="POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        pass1 = request.POST.get("pass1")
+        pass2 = request.POST.get("pass2")
+        if pass1 != pass2:
+            return render(request, "recommender/signup.html", {"passdidnotmatch": True})
+        if username and email and pass1:
+            user = User.objects.create_user(username, email, pass1)
+            user.save()
+            return render(request, "recommender/login.html", {"usercreated": True, "user_full_name": username})
+    return render(request, "recommender/signup.html", {})
+
+def login_page(request):
+    return render(request, "recommender/login.html", {})
